1db1500d628dc4f1773ccbf9945fa6876f6ad5ca
batista7557 <gabriel.bat2001@gmail.com>
2023-02-21 23:08:09 -0300

    Rotacionando as figuras

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def check_borders():
     return True
 
 while True:
-    dx = 0
+    dx, rotate = 0, False
     game_sc.fill(pygame.Color('black'))
 
     #controle
@@ -50,7 +50,8 @@ while True:
                 dx = 1
             elif event.key == pygame.K_DOWN:
                 anim_limit = 100
-
+            elif event.key == pygame.K_UP:
+                rotate = True
         #Mover o x
         figure_old = deepcopy(figure)
         for i in range(4):
@@ -72,6 +73,19 @@ while True:
                     figure = deepcopy(figure_old)
                     anim_limit = 2000
                     break
+
+    #Rotação
+    center = figure[0]
+    figure_old = deepcopy(figure)
+    if rotate:
+        for i in range(4):
+            x = figure[i].y - center.y
+            y = figure[i].x - center.x
+            figure[i].x = center.x - x
+            figure[i].y = center.y + y
+            if not check_borders():
+                figure = deepcopy(choice(figures))
+                break
 
     #Desenhar grid
     [pygame.draw.rect(game_sc, (40, 40, 40), i_rect, 1) for i_rect in grid]
